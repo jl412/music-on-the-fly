1560532604a2f00fa0d99c2b1802dfbc483b8334
jl412 <liqiangjohnny@gmail.com>
2017-08-17 01:50:24 -0400

    mongo string

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -5,7 +5,7 @@ if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157282.mlab.com:57282/heroku_3z32f873'; // user yours
+    connectionString += '@ds149373.mlab.com:49373/heroku_vxs6jxjz'; // user yours
     console.log("mongo running remotely");
 }
 
